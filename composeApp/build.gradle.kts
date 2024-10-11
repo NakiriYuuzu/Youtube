43a23d6f40a058c95d056a694b2c531213465b2c
herrytsai <herrytsai@gis.tw>
2024-10-11 12:06:52 +0800

    ✨Init Commits.

---
+++ composeApp/build.gradle.kts
@@ -0,0 +1,78 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlinSerialization)
+    alias(libs.plugins.ksp)
+}
+
+kotlin {
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        moduleName = "composeApp"
+        browser {
+            val rootDirPath = project.rootDir.path
+            val projectDirPath = project.projectDir.path
+            commonWebpackConfig {
+                outputFileName = "composeApp.js"
+                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
+                    static = (static ?: mutableListOf()).apply {
+                        // Serve sources to debug inside browser
+                        add(rootDirPath)
+                        add(projectDirPath)
+                    }
+                }
+            }
+        }
+        binaries.executable()
+    }
+
+    js(IR) {
+        moduleName = "composeApp"
+        browser {
+            commonWebpackConfig {
+                outputFileName = "composeApp.js"
+            }
+        }
+        binaries.executable()
+    }
+    
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.ui)
+            implementation(compose.runtime)
+            implementation(compose.material3)
+            implementation(compose.foundation)
+            implementation(compose.runtimeSaveable)
+            implementation(compose.components.resources)
+            implementation(compose.materialIconsExtended)
+            implementation(compose.components.uiToolingPreview)
+
+            implementation(libs.bundles.androidx)
+            implementation(libs.bundles.kotlinx)
+            implementation(libs.bundles.ui)
+            implementation(libs.bundles.network)
+            implementation(libs.bundles.database)
+            implementation(libs.bundles.di)
+            implementation(libs.bundles.debug)
+        }
+        jsMain.dependencies {
+            implementation(libs.ktor.client.js)
+        }
+        wasmJsMain.dependencies {
+            implementation(libs.ktor.client.wasmjs)
+        }
+//        val wasmJsMain by getting {
+//            dependencies {
+//            }
+//        }
+    }
+
+    jvmToolchain(17)
+}
+
+
---
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -0,0 +1,10 @@
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+
+@Composable
+fun App() {
+    MaterialTheme {
+
+    }
+}
